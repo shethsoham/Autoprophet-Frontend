0c003d80f7120d91e764327e83719e41b1dece79
Soham Sheth <sohamashishsheth@gmail.com>
2025-01-22 01:41:06 -0600

    Login page design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const user = useSelector((store) => store.user);
+  console.log(user);
+
   return (
-    <div>
-      <div className="navbar bg-base-300">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl"> 📊Autoprophet</a>
-        </div>
+    <div className="navbar bg-base-300">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">
+          👩‍💻 DevTinder
+        </Link>
+      </div>
+      {user && (
         <div className="flex-none gap-2">
-          <div className="form-control"></div>
-          <div className="dropdown dropdown-end mx-5">
+          <div className="form-control">Welcome, {user.firstName}</div>
+          <div className="dropdown dropdown-end mx-5 flex">
             <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
+                <img alt="user photo" src={user.photoUrl} />
               </div>
             </div>
             <ul
@@ -27,10 +30,10 @@ const NavBar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
@@ -41,9 +44,8 @@ const NavBar = () => {
             </ul>
           </div>
         </div>
-      </div>
+      )}
     </div>
-  )
-}
-
-export default NavBar
+  );
+};
+export default NavBar;
